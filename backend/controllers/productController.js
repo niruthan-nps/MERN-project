cee094c2ded1198468511d60fbaa8ec07ea302fa
niruthan-nps <niruthanparanthaman@gmail.com>
2025-05-14 00:40:59 +0530

    Add newProduct function to handle product creation

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,17 @@
+const Product = require('../models/productModel');
+
+
 exports.getProducts = (req,res,next) => {
     res.status(200).json({
         success: true,
         message: 'List of all products'
     });
+}
+exports.newProduct = async (req,res,next) => {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
+    });
+   
 }
