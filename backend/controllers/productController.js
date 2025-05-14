47c04176e6eb314cb5039ac426d2afa97b3d388d
niruthan-nps <niruthanparanthaman@gmail.com>
2025-05-14 15:27:09 +0530

    Fix route and function name for creating new products(products - product)

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,8 +7,8 @@ exports.getProducts = (req,res,next) => {
         message: 'List of all products'
     });
 }
-//create new product - api/v1/products/new
-exports.newProducts = async (req,res,next) => {
+//create new product - api/v1/product/new
+exports.newProduct = async (req,res,next) => {
     const product = await Product.create(req.body);
     res.status(201).json({
         success: true,
