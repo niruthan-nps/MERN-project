5bf2f88e15b0066e9aa70af94908986e6e0d55c3
niruthan-nps <niruthanparanthaman@gmail.com>
2025-05-15 00:38:26 +0530

    Add custom error handling and improve product not found response

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require('../models/productModel');
+const ErrorHandler = require('../utils/errorHandler');
 
 //get all products - api/v1/products
 exports.getProducts = async (req,res,next) => {
@@ -26,11 +27,9 @@ exports.getSingleProduct = async (req,res,next) => {
     const product = await Product.findById(req.params.id);
     
     if(!product){
-    return res.status(404).json({
-        success: false,
-        message: 'Product not found'
-    });
-}
+        new ErrorHandler('Product not found', 400);
+    };
+
     res.status(201).json({
         success: true,
         product
