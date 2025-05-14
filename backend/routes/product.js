b0085dc5fff1adf7f5909d64d2219ab738eb8d8d
niruthan-nps <niruthanparanthaman@gmail.com>
2025-05-14 15:27:09 +0530

    Fix route and function name for creating new products(products - product)

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,12 +1,12 @@
 const express = require('express');//import express module
 const { getProducts } = require('../controllers/productController');
-const { newProducts } = require('../controllers/productController');//importing the newProducts function from productController file
+const { newProduct } = require('../controllers/productController');//importing the newProducts function from productController file
 const router = express.Router();//Hey Express, give me a new Router object so I can define some grouped routes in it.
 
 router.route('/products').get(getProducts)
 //router is the object created from Express module's one of the funtion named Router and the now we  are accessing route function from previously created router object
 
-router.route('/products/new').post(newProducts)
+router.route('/product/new').post(newProduct)
 
 
 
