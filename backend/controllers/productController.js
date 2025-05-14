3372ea284bd5c6177445b10139a8912fba8a6e75
niruthan-nps <niruthanparanthaman@gmail.com>
2025-05-14 16:46:09 +0530

    Refactor getProducts function to include product count and list in response

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,13 @@
 const Product = require('../models/productModel');
 
+//get all products - api/v1/products
+exports.getProducts = async (req,res,next) => {
 
-exports.getProducts = (req,res,next) => {
+    const products = await Product.find();
     res.status(200).json({
         success: true,
-        message: 'List of all products'
+        count : products.length,
+        products
     });
 }
 //create new product - api/v1/product/new
@@ -15,4 +18,20 @@ exports.newProduct = async (req,res,next) => {
         product
     });
    
+}
+
+//get single product - api/v1/product/:id
+exports.getSingleProduct = async (req,res,next) => {
+    const product = await Product.findById(req.params.id);
+    
+    if(!product){
+    return res.status(404).json({
+        success: false,
+        message: 'Product not found'
+    });
+}
+    res.status(201).json({
+        success: true,
+        product
+    });
 }
