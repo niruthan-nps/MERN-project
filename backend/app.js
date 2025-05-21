afde972aeca6bc0335e8804517e792f234e41b0c
niruthan-nps <niruthanparanthaman@gmail.com>
2025-05-21 09:38:59 +0530

    add auth route integration in app.js

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,13 @@ const app = express();
 const errorMiddleware = require('./middlewares/error');
 
 
+
 app.use(express.json());
 const products = require('./routes/product');
+const auth = require('./routes/auth');
 
 app.use('/api/v1', products);
+app.use('/api/v1', auth);
 
 app.use(errorMiddleware);
 
