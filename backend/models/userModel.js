25e74ecd54065aa8dc17eea916bd96167b4e19cd
niruthan-nps <niruthanparanthaman@gmail.com>
2025-05-21 10:12:45 +0530

    Password hashed using bycrypt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -33,6 +34,12 @@ const userSchema = new mongoose.Schema({
         default: Date.now
     }
 })
+
+userSchema.pre('save', async function(next){
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+
 
 let model = mongoose.model('User', userSchema);
 module.exports = model;
