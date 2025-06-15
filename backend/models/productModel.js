e2255c22fed511b3af6295e92335c9f52356a5bc
niruthan-nps <niruthanparanthaman@gmail.com>
2025-06-15 23:54:10 +0530

    feat: associate user with product creation by adding user field to product model

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -86,6 +86,9 @@ const productSchema = new mongoose.Schema({
             }
         }
     ],
+    user:{
+        type : mongoose.Schema.Types.ObjectId
+    },
     createdAt: {
         type: Date,
         default: Date.now,
