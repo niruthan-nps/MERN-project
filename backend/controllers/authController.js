66277b009411e0d6c3bd3282f14a11dc479b26ca
niruthan-nps <niruthanparanthaman@gmail.com>
2025-06-16 12:46:53 +0530

    feat: implement reset password functionality with token validation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,6 +3,7 @@ const User = require('../models/userModel')
 const sendEmail = require('../utils/email')
 const ErrorHandler = require('../utils/errorHandler')
 const sendToken = require('../utils/jwt')
+const crypto = require('crypto')
 
 
 exports.registerUser = catchAsyncError(async (req,res,next) => {
@@ -82,4 +83,34 @@ exports.forgotPassword = catchAsyncError( async ( req,res,next) => {
         await user.save({ validateBeforeSave: false});
         return next(new ErrorHandler(error.message), 500);
      }
+})
+
+
+exports.resetPassword = catchAsyncError(async (req,res,next) => {
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
+    //createHash('sha256') creates a hash of the token using SHA-256 algorithm
+    //update(req.params.token) updates the token with the token from the request parameters
+    
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordTokenExpire: { $gt: Date.now() }
+    }); //check if the token is valid and not expired
+    // $gt means greater than, so we are checking if the token is still valid (not expired)
+
+    if(!user){
+        return next(new ErrorHandler('Reset Password Token is invalid or has expired', 400));
+    }
+
+    if(req.body.password !== req.body.confirmPassword){
+        return next(new ErrorHandler('Passwords do not match', 400));
+    }
+
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordTokenExpire = undefined;
+
+    await user.save({validateBeforeSave: false});
+
+    sendToken(user, 201, res);
 })
