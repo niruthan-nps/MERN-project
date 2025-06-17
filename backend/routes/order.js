a6323d264c29d384ae00d1f3ec1fee8428a8145d
niruthan-nps <niruthanparanthaman@gmail.com>
2025-06-18 00:02:27 +0530

    feat: add get single order and my orders functionality with routes

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {newOrder} = require('../controllers/orderController');
+const {newOrder, getSingleOrder, myOrders} = require('../controllers/orderController');
 const {isAuthenticatedUser} = require('../middlewares/authenticate');
 
 router.route('/orders/new').post( isAuthenticatedUser, newOrder);
+router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
+router.route('/myorders').get(isAuthenticatedUser, myOrders);
 module.exports = router;
