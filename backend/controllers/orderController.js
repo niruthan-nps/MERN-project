71579d111d14581bbeff1d3eb8d38015f7370ca3
niruthan-nps <niruthanparanthaman@gmail.com>
2025-06-18 00:02:27 +0530

    feat: add get single order and my orders functionality with routes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,6 @@
 const catchAsyncError = require('../middlewares/catchAsyncError');
 const Order = require('../models/orderModel');
+const ErrorHandler = require('../utils/errorHandler');
 
 
 //create new ordre - api/v1/orders/new
@@ -31,6 +32,36 @@ exports.newOrder = catchAsyncError(async (req, res, next) => {
     res.status(201).json({
         success: true,
         order
+    });
+
+})
+
+
+//get single order details - api/v1/order/:id
+exports.getSingleOrder = catchAsyncError(async (req, res, next) => {
+
+    const order = await Order.findById(req.params.id).populate('user', 'name email');
+
+    if(!order){
+        return next(new ErrorHandler(`Order does not exist with id: ${req.params.id}`, 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        order
+    });
+
+})
+
+
+//get logged in user orders - api/v1/orders/myorders
+exports.myOrders = catchAsyncError(async (req, res, next) => {
+
+    const orders = await Order.find({ user: req.user.id });
+
+    res.status(200).json({
+        success: true,
+        orders
     });
 
 })
