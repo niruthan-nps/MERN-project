63e9a1439a712858d30f4be7eec9a212c13e098e
niruthan-nps <niruthanparanthaman@gmail.com>
2025-06-18 11:20:08 +0530

    feat: add delete order functionality for admin

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {newOrder, getSingleOrder, myOrders, orders, updateOrder} = require('../controllers/orderController');
+const {newOrder, getSingleOrder, myOrders, orders, updateOrder, deleteOrder} = require('../controllers/orderController');
 const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/authenticate');
 
 router.route('/orders/new').post( isAuthenticatedUser, newOrder);
@@ -11,5 +11,7 @@ router.route('/myorders').get(isAuthenticatedUser, myOrders);
 
 router.route('/orders').get(isAuthenticatedUser, authorizeRoles('admin'), orders); // Assuming you want to list all orders for user for admin
 router.route('/order/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateOrder);
+router.route('/order/:id').delete(isAuthenticatedUser, authorizeRoles('admin'), deleteOrder);
+
 
 module.exports = router;
