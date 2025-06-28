bfce122074fad0f855830d29b1fcdb689c0c470a
niruthan-nps <niruthanparanthaman@gmail.com>
2025-06-28 13:01:16 +0530

    feat: implement routing in App component with Header, Home, and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
-
 import './App.css';
 import Footer from './components/layouts/Footer';
 import Header from './components/layouts/Header';
 import Home from './components/layouts/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Add other routes here */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
