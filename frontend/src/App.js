7a7a738e26d3a1eb1a4df7ae51870290fe639caa
niruthan-nps <niruthanparanthaman@gmail.com>
2025-06-28 12:25:49 +0530

    feat: add Footer component to enhance app layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Footer from './components/layouts/Footer';
 import Header from './components/layouts/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
