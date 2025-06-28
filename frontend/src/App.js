a9087f1fac92e4db368e79ac1b4ff38f3ed88147
niruthan-nps <niruthanparanthaman@gmail.com>
2025-06-28 12:53:54 +0530

    feat: add Home component with product listings and images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Footer from './components/layouts/Footer';
 import Header from './components/layouts/Header';
+import Home from './components/layouts/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Footer />
     </div>
   );
