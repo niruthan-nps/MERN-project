192628c8dbd29bfc4be190507f18f0244d9c49f1
niruthan-nps <niruthanparanthaman@gmail.com>
2025-07-01 00:30:27 +0530

    feat: add Loader component and Product component for product display; update Home component to use new structure

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,6 +4,8 @@ import MetaData from "./layouts/MetaData";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getProducts } from "../actions/productsActions";
+import Loader from "./layouts/Loader";
+import Product from "./product/Product";
 
 export default function Home() {
 
@@ -18,38 +20,25 @@ export default function Home() {
   return (
     <Fragment>
 
-            
-        <h1 id="products_heading">Latest Products</h1>
+    {loading ? <Loader /> : 
 
-        <MetaData title={'Products'} />
+        <Fragment>
 
-            <section id="products" className="container mt-5">
-                <div className="row">
-                    {products && products.map((product) => (
-                        <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-                    <div className="card p-3 rounded">
-                        <img
-                        className="card-img-top mx-auto"
-                        src={product.images[0].image}
-                        />
-                        <div className="card-body d-flex flex-column">
-                        <h5 className="card-title">
-                            <a href="">{product.name}</a>
-                        </h5>
-                        <div className="ratings mt-auto">
-                            <div className="rating-outer">
-                            <div className="rating-inner" style={{ width: `${(product.ratings / 5) * 100}%` }}></div>
-                            </div>
-                            <span id="no_of_reviews">( {product.numOfReviews} Reviews)</span>
-                        </div>
-                        <p className="card-text">{product.price}</p>
-                        <a href="#" id="view_btn" className="btn btn-block">View Details</a>
-                        </div>
+                
+            <h1 id="products_heading">Latest Products</h1>
+
+            <MetaData title={'Products'} />
+
+                <section id="products" className="container mt-5">
+                    <div className="row">
+                        {products && products.map((product) => (
+                        <Product product={product}/>
+                    ))}
                     </div>
-                    </div>))}
-                </div>
-            </section>
+                </section>
 
+        </Fragment>
+    }
     </Fragment>
   );
 }
