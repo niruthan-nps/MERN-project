79d5db945b5f75f95350950f44ccfe82c84b59ff
niruthan-nps <niruthanparanthaman@gmail.com>
2025-07-11 12:19:17 +0530

    feat: add order management functionality; implement order actions and slice, and integrate into store

---
+++ frontend/src/slices/orderSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const orderSlice = createSlice({
+  name: "order",
+  initialState: {
+    orderDetail:{},
+    userOrder:[],
+    loading:false
+    },
+      reducers: {
+        createOrderRequest(state, action){
+            return{
+                ...state,
+                loading:true
+            }
+
+            },
+            createOrderSuccess(state, action){
+            return{
+                ...state,
+                loading:false,
+                orderDetail:action.payload
+            }
+
+            },
+            createOrderFail(state, action){
+            return{
+               ...state,
+                loading:false,
+                error:action.payload
+            }
+
+            }
+      }
+
+})
+
+
+const { actions, reducer } = orderSlice;
+export const { createOrderRequest, createOrderSuccess, createOrderFail } = actions;
+export default reducer;
